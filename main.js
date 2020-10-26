9244f02e98a3165e141014724e7cccce3c96e659
Stanislao <lombardo.stanislao94@xn--lieb-spa>
2020-10-26 15:41:57 +0100

    per ticket

---
+++ main.js
@@ -0,0 +1,8 @@
+// Al click sull'icona dell'hamburger menu, deve essere visualizzato il menu mobile e al click sull'icona "X" il menu mobile deve scomparire.
+
+// Intercetto il click sull'hamburger menu
+
+$('a i.fa-bars').click(function()) {
+    alert('click');
+
+}
